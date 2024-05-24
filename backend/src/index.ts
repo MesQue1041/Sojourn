e125e413c2a35d43b86643cf9d6fcaecba528b75
MesQue1041 <dovebookt@gmail.com>
2024-05-24 21:42:43 +0530

    register backend working

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,7 @@ mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors()); // Consider configuring CORS for specific origins if necessary
+app.use(cors()); 
 
 app.use("/api/users", userRoutes);
 
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,35 +1,46 @@
 import express, { Request, Response } from "express";
 import User from "../models/User";
 import jwt from "jsonwebtoken";
+import { check, validationResult } from "express-validator";
 
 const router = express.Router();
 
 // /api/users/register
-router.post("/register", async (req: Request, res: Response) => {
+router.post("/register", [
+  check("firstName", "First Name is required").isString(),
+  check("lastName", "Last Name is required").isString(),
+  check("email", "Email is required").isEmail(),
+  check("password", "Password with 6 or more characters required").isLength({ min: 6 }),
+], async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    // If there are validation errors, return them in the response body
+    return res.status(400).json({ errors: errors.array() });
+  }
   try {
-    const { email, password, firstName, lastName } = req.body;
+    let user = await User.findOne({ email: req.body.email });
 
-
-    let existingUser = await User.findOne({ email });
-    if (existingUser) {
+    if (user) {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    const newUser = new User({ email, password, firstName, lastName });
-    await newUser.save();
+    user = new User(req.body);
+    await user.save();
 
-    const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET_KEY as string, { expiresIn: "1d" });
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET_KEY as string, { expiresIn: "1d" });
 
     res.cookie("auth_token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      maxAge: 86400000, 
+      maxAge: 86400000,
     });
 
-    return res.status(201).json({ message: "User created successfully" }); 
+    // If registration is successful, return a success message
+    return res.status(201).json({ message: "User created successfully" });
+
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server Error" });
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
   }
 });
 
